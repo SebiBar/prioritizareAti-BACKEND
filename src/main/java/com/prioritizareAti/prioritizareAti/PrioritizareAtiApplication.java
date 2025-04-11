65ae3f101dcf5c4143960963fd1ff21e078b64ba
Barta Sebastian <sebibarta11@gmail.com>
2025-04-11 20:37:42 +0300

    first commit

---
+++ src/main/java/com/prioritizareAti/prioritizareAti/PrioritizareAtiApplication.java
@@ -0,0 +1,13 @@
+package com.prioritizareAti.prioritizareAti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrioritizareAtiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrioritizareAtiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prioritizareAti/prioritizareAti/PrioritizareAtiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prioritizareAti.prioritizareAti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrioritizareAtiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
